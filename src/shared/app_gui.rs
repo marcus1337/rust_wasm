788d0e9b0b6219981699f1b48ddbd245b15c9c26
marcus1337 <blurgas@hotmail.com>
2024-05-27 20:28:30 +0200

    egui

---
+++ src/shared/app_gui.rs
@@ -0,0 +1,25 @@
+use bevy::prelude::*;
+use bevy_egui::{egui, EguiContexts, EguiPlugin};
+
+pub fn initialize_gui(app: &mut App) {
+    app.add_plugins(EguiPlugin);
+    app.add_systems(Update, ui_main_menu_system);
+}
+
+fn ui_main_menu_system(mut egui_contexts: EguiContexts) {
+
+    let window_color = egui::Color32::from_rgba_premultiplied(128, 128, 128, 200);
+    
+    egui::Window::new("Main Menu")
+    .title_bar(false)
+    .resizable(true)
+    .collapsible(false)
+    .frame(egui::Frame{fill: window_color, ..Default::default()})
+    .show(egui_contexts.ctx_mut(), |ui| {
+        ui.label(egui::RichText::new("Menu!").heading().color(egui::Color32::from_rgb(255, 255, 0)));
+        ui.add_space(20.0);
+        if ui.button("Button").clicked() {
+            println!("Button clicked");
+        }
+    });
+}
--- src/shared/mod.rs
+++ src/shared/mod.rs
@@ -1,51 +1,15 @@
-use bevy::asset::AssetMetaCheck;
+
+mod app_init;
+mod app_gui;
 use bevy::prelude::*;
-use bevy::window::{PresentMode, Window, WindowPlugin, WindowTheme};
+use app_init::initialize_app;
+use app_gui::initialize_gui;
+
 
 pub fn init_and_run(canvas_id: Option<String>) {
     let mut app = App::new();
     initialize_app(&mut app, canvas_id.clone());
+    initialize_gui(&mut app);
     app.run();
 }
 
-fn initialize_app(app: &mut App, canvas_id: Option<String>) {
-    app.insert_resource(AssetMetaCheck::Never);
-    initialize_app_plugins(app, canvas_id);
-    app.add_systems(Startup, setup);
-    app.insert_resource(ClearColor(Color::rgb(0.3, 0.2, 0.2)));
-}
-
-fn initialize_app_plugins(app: &mut App, canvas_id: Option<String>) {
-    //app.add_plugins(DefaultPlugins);
-    app.add_plugins(DefaultPlugins.set(WindowPlugin {
-        primary_window: Some(Window {
-            title: "My_Bevy_Window!".into(),
-            canvas: canvas_id,
-            resolution: (1920., 1080.).into(),
-            present_mode: PresentMode::AutoVsync,
-            window_theme: Some(WindowTheme::Dark),
-            resizable: true,
-            ..default()
-        }),
-        ..default()
-    }));
-}
-
-fn setup(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut _materials: ResMut<Assets<ColorMaterial>>,
-) {
-    commands.spawn(Camera2dBundle::default());
-    commands.spawn(Text2dBundle {
-        text: Text::from_section(
-            "Hello World åäö ÅÄÖ",
-            TextStyle {
-                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                font_size: 60.0,
-                color: Color::WHITE,
-            },
-        ),
-        ..Default::default()
-    });
-}
