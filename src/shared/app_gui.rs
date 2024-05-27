e6a32e637f8758803b85880d03f30f4b3208ad73
marcus1337 <blurgas@hotmail.com>
2024-05-27 21:09:12 +0200

    egui

--- src/shared/app_gui.rs
+++ src/shared/app_gui.rs
@@ -9,15 +9,22 @@ pub fn initialize_gui(app: &mut App) {
 fn ui_main_menu_system(mut egui_contexts: EguiContexts) {
 
     let window_color = egui::Color32::from_rgba_premultiplied(128, 128, 128, 200);
-    
+    let sr = egui_contexts.ctx_mut().screen_rect();
+    let w = sr.width();
+    let h = sr.height();
+
     egui::Window::new("Main Menu")
     .title_bar(false)
     .resizable(true)
     .collapsible(false)
+    .fixed_pos(egui::pos2(300.0,50.0))
     .frame(egui::Frame{fill: window_color, ..Default::default()})
     .show(egui_contexts.ctx_mut(), |ui| {
-        ui.set_width(500.0);
-        ui.set_height(300.0);
+        let pixels_per_point = ui.ctx().pixels_per_point();
+        let width = 0.5 * w;
+        let height = 300.0 * pixels_per_point;
+        ui.set_width(width);
+        ui.set_height(height);
         ui.label(egui::RichText::new("Menu!").heading().color(egui::Color32::from_rgb(255, 255, 0)));
         ui.add_space(20.0);
         if ui.button("Button").clicked() {
