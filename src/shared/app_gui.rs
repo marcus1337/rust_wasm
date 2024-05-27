73c768cfaee74f972753dce3dc89641266cef945
marcus1337 <blurgas@hotmail.com>
2024-05-27 20:54:22 +0200

    egui

--- src/shared/app_gui.rs
+++ src/shared/app_gui.rs
@@ -16,6 +16,8 @@ fn ui_main_menu_system(mut egui_contexts: EguiContexts) {
     .collapsible(false)
     .frame(egui::Frame{fill: window_color, ..Default::default()})
     .show(egui_contexts.ctx_mut(), |ui| {
+        ui.set_width(500.0);
+        ui.set_height(300.0);
         ui.label(egui::RichText::new("Menu!").heading().color(egui::Color32::from_rgb(255, 255, 0)));
         ui.add_space(20.0);
         if ui.button("Button").clicked() {
